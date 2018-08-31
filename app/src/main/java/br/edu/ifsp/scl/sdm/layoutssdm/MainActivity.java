0fc285d68e522f1f6a3147b096cf1ca6ca4b0127
Pedro Northon Nobile <nobile.pedro@gmail.com>
2018-08-31 11:25:16 -0300

    Segundo commit com RelativeLayout e tratamento de evento do CheckBox com base na mudança de estado.

--- app/src/main/java/br/edu/ifsp/scl/sdm/layoutssdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/layoutssdm/MainActivity.java
@@ -2,12 +2,35 @@ package br.edu.ifsp.scl.sdm.layoutssdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    private CheckBox notificacoesCheckBox;
+    private RadioGroup notificacoesRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.frame_layout_activity_main);
+        setContentView(R.layout.relative_layout_activity_main);
+
+        // Buscando referências para componentes visuais
+        notificacoesCheckBox = findViewById(R.id.notificacoesCheckBox);
+        notificacoesRadioGroup = findViewById(R.id.notificacoesRadioGroup);
+
+        // Tratamento de eventos de baseado na mudança de estado
+        notificacoesCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    notificacoesRadioGroup.setVisibility(View.VISIBLE);
+                }
+                else {
+                    notificacoesRadioGroup.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
